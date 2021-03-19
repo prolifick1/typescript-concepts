058fa7d677ceb0219343ce8e8744b817f6d38598
Luis Manzo <manzosk3@gmail.com>
2021-03-18 20:14:28 -0400

    adds solution to interface lesson

--- intro-to-interface.js
+++ intro-to-interface.js
@@ -1,3 +1,4 @@
+//create an interface that describest the shape of the operate object
 function operate(x) {
     return x.side * x.side;
 }
@@ -5,3 +6,4 @@ var calc = operate({
     shape: 'square',
     side: 5
 });
+console.log(calc);
--- intro-to-interface.ts
+++ intro-to-interface.ts
@@ -1,10 +1,17 @@
 //create an interface that describest the shape of the operate object
 
-function operate(x:{shape: string; side: number}) {
+interface operateInterface{
+  shape: string,
+  side?: number
+}
+
+function operate(x:operateInterface) {
   return x.side * x.side;
 }
 
 var calc = operate({
   shape: 'square',
   side: 5
-})
+});
+
+console.log(calc);
