d067a28350324ce794db341d63cab9b9f5e940de
Luis Manzo <manzosk3@gmail.com>
2021-03-26 17:13:23 -0400

    adds codevolution functions examples

---
+++ codevolution/functions.js
@@ -0,0 +1,22 @@
+// function add(num1: number, num2: number): number {
+//   return num1 + num2;
+// }
+// add(5, 10);
+//OPTIONAL PARAMETER
+// function add(num1: number, num2?: number): number {
+//   if(num2) {
+//     return num1 + num2;
+//   } else {
+//     return num1;
+//   }
+// }
+// console.log(add(5));
+function add(num2, num1) {
+    if (num1) {
+        return num1 + num2;
+    }
+    else {
+        return num2;
+    }
+}
+console.log(add(4));
---
+++ codevolution/functions.ts
@@ -0,0 +1,25 @@
+// function add(num1: number, num2: number): number {
+//   return num1 + num2;
+// }
+
+// add(5, 10);
+
+//OPTIONAL PARAMETER
+// function add(num1: number, num2?: number): number {
+//   if(num2) {
+//     return num1 + num2;
+//   } else {
+//     return num1;
+//   }
+// }
+// console.log(add(5));
+
+//note: for optional parameters must be declared after required parameters
+// function add(num2: number, num1?: number) {
+//   if(num1) {
+//     return num1 + num2;
+//   } else {
+//     return num2;
+//   }
+// }
+// console.log(add(4));
