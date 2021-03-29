cea488a87311f4e6c83cabbf968fcc20bdb9e0c2
Luis Manzo <manzosk3@gmail.com>
2021-03-29 12:57:52 -0400

    adds typecast function example

--- traversymedia/functions.js
+++ traversymedia/functions.js
@@ -1,5 +1,16 @@
 //create function getSum, specifying parameter types and return type
-function getSum(num1, num2) {
+// function getSum(num1: number, num2: number) : number {
+//   return num1 + num2;
+// }
+// console.log(getSum(4, 4));
+//create a function expression that accepts any type as parameters, casts the parameters to number type, and returns number
+var mySum = function getSum(num1, num2) {
+    if (typeof (num1) === 'string') {
+        num1 = Number(num1);
+    }
+    if (typeof (num2) === 'string') {
+        num2 = Number(num2);
+    }
     return num1 + num2;
-}
-console.log(getSum(1, 4));
+};
+console.log(mySum('3', 5));
--- traversymedia/functions.ts
+++ traversymedia/functions.ts
@@ -1,7 +1,20 @@
 //create function getSum, specifying parameter types and return type
 
-function getSum(num1, num2) {
+// function getSum(num1: number, num2: number) : number {
+//   return num1 + num2;
+// }
+
+// console.log(getSum(4, 4));
+
+//create a function expression that accepts any type as parameters, casts the parameters to number type, and returns number
+let mySum = function getSum(num1: any, num2: any): number {
+  if (typeof (num1) === 'string') {
+    num1 = Number(num1);
+  }
+  if(typeof(num2) === 'string') {
+    num2 = Number(num2);
+  }
   return num1 + num2;
 }
 
-console.log(getSum(1, 4));
+console.log(mySum('3', 5));
