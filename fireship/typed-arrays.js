4766cae307642860b38a2512ecf07dc737f4cb26
Luis Manzo <manzosk3@gmail.com>
2021-03-29 13:28:02 -0400

    adds fireship typed arrays example

---
+++ fireship/typed-arrays.js
@@ -0,0 +1,6 @@
+//sets up fireship typed arrays example
+var arr = [];
+arr.push(1);
+arr.push('23');
+arr.push(false);
+console.log(arr);
---
+++ fireship/typed-arrays.ts
@@ -0,0 +1,9 @@
+//sets up fireship typed arrays example
+
+const arr = [];
+
+arr.push(1);
+arr.push('23');
+arr.push(false);
+
+console.log(arr);
